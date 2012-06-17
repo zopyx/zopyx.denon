cbc9c6b51d42ec2da591b62dccb69b61e5b1a683
zopyx <yet@gmx.de>
2012-06-17 17:25:35 +0200

    filepath fixes

--- zopyx/denon/denon.py
+++ zopyx/denon/denon.py
@@ -1,17 +1,22 @@
 import os
 from bottle import route, run, template
-from bottle import static_file
+from bottle import static_file, default_app
+
+cwd = os.path.dirname(__file__)
 
 @route('/static/<filename>')
 def server_static(filename):
-        return static_file(filename, root=os.getcwd())
+        return static_file(filename, root=cwd)
 
 @route('/denon')
 def denon():
-    return template('denon.pt')
+    return template(os.path.join(cwd, 'denon.pt'))
 
 def main():
-    run(host='0.0.0.0', port=10000)
+#    run(host='0.0.0.0', port=10000)
+    application = default_app()
+    from paste import httpserver
+    httpserver.serve(application, host='0.0.0.0', port=10000)
 
 if __name__ == '__main__':
     main()
